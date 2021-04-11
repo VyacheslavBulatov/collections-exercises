3feaf8b5f38c6a07d5f25740978db34a1623985d
VadimBolokhov <wad4@inbox.ru>
2021-04-11 12:34:06 +0300

    Simple ArrayList

---
+++ src/main/java/ru/vbolokhov/collections/SimpleArrayList.java
@@ -0,0 +1,29 @@
+package ru.vbolokhov.collections;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * @author Vadim Bolokhov
+ */
+public class SimpleArrayList<T> {
+
+    public boolean add(T element) {
+        throw new NotImplementedException();
+    }
+
+    public void add(int index, T element) {
+        throw new NotImplementedException();
+    }
+
+    public int size() {
+        throw new NotImplementedException();
+    }
+
+    public boolean remove(T element) {
+        throw new NotImplementedException();
+    }
+
+    public T remove(int index) {
+        throw new NotImplementedException();
+    }
+}
